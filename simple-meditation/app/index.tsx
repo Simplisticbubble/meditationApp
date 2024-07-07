b8805cb18717026cf6be75359202b43cf6615240
Simplisticbubble <ks.kevinseu@gmail.com>
2024-07-07 13:10:03 +1000

    added affirmation tabs

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 import { LinearGradient} from 'expo-linear-gradient'
 import beachImage from "@/assets/meditation-images/beach.webp";
 import CustomButton from '@/components/CustomButton';
+import { useRouter } from 'expo-router';
+import AppGradient from '@/components/AppGradient';
 
 const App = () => {
+  const router = useRouter();
   return (
     <View className='flex-1'>
       <ImageBackground source={beachImage} resizeMode='cover' className='flex-1'>
-        <LinearGradient className = "flex-1" colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
-          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
+        <AppGradient colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}>
+          <SafeAreaView className='flex-1 px-1 justify-between'>
             <View>
           <Text className='text-center text-white font-bold text-4xl mt-12'>Simple Meditation</Text>
           <Text className='text-center text-white text-regular text-2xl mt-3'>
@@ -18,14 +21,15 @@ const App = () => {
           </Text>
           </View>
           <View>
-            <CustomButton onPress = {()=>console.log('tap')} title = "Get Started"/>
+            <CustomButton onPress = {()=>router.push('/nature-meditate')}
+                           title = "Get Started"/>
           </View>
 
           <StatusBar style="light"/>
           </SafeAreaView>
           
           
-        </LinearGradient>
+        </AppGradient>
 
       </ImageBackground>
       
