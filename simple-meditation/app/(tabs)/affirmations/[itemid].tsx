f740477aa74ded48170f4f13d6fa38ed0ce790b0
Simplisticbubble <ks.kevinseu@gmail.com>
2024-07-07 13:36:37 +1000

    added readable text to affirmation items

--- simple-meditation/app/(tabs)/affirmations/[itemid].tsx
+++ simple-meditation/app/(tabs)/affirmations/[itemid].tsx
@@ -1,19 +1,27 @@
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, Pressable, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { useLocalSearchParams, router } from 'expo-router'
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
 import AppGradient from '@/components/AppGradient';
+import { AntDesign } from '@expo/vector-icons';
 
 const AffirmationPractice = () => {
     const {itemid} = useLocalSearchParams();
     const [affirmation, setAffirmation] =  useState<GalleryPreviewData>();
+    const [sentences, setSentences] = useState<string[]>([]);
     useEffect(()=> {
         for (let idx = 0; idx <AFFIRMATION_GALLERY.length; idx++){
             const affirmationData = AFFIRMATION_GALLERY[idx].data;
             const affirmationToStart = affirmationData.find((a)=>a.id === Number(itemid));
             if(affirmationToStart){
                 setAffirmation(affirmationToStart);
+                const affirmationArray = affirmationToStart.text.split(".");
+
+                if (affirmationArray[affirmationArray.length -1] === ''){
+                    affirmationArray.pop();
+                }
+                setSentences(affirmationArray)
                 return;
             }
 
@@ -23,7 +31,20 @@ const AffirmationPractice = () => {
         <View className='flex-1'>
             <ImageBackground source={affirmation?.image} resizeMode='cover' className='flex-1'>
                 <AppGradient colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.9)"]}>
-                    <Text>Test</Text>
+                    <Pressable onPress= {()=>router.back()} className='absolute top-16 left-6 z-10'>
+                        <AntDesign name="leftcircleo" size={50} color="white"/>
+                    </Pressable>
+                    <ScrollView className='mt-20' showsVerticalScrollIndicator={false}>
+                        <View className='h-full justify-center'>
+                            <View className='h-4/5 justify-center'>
+                                {sentences?.map((sentence, idx)=>(
+                                    <Text className='text-white text-3xl mb-12 font-bold text-center'> {sentence}.</Text>
+                                ))}
+                                
+                               
+                            </View>
+                        </View>
+                    </ScrollView>
                 </AppGradient>
             </ImageBackground>
         </View>
