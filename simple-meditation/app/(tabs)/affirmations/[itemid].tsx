c120c0543bc2ed9c7bb90eb311a440ea2acdcc9e
Simplisticbubble <ks.kevinseu@gmail.com>
2024-07-07 13:24:52 +1000

    added affirmation images

--- simple-meditation/app/(tabs)/affirmations/[itemid].tsx
+++ simple-meditation/app/(tabs)/affirmations/[itemid].tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
+import AppGradient from '@/components/AppGradient';
 
 const AffirmationPractice = () => {
     const {itemid} = useLocalSearchParams();
@@ -20,7 +21,11 @@ const AffirmationPractice = () => {
     }, [])
     return (
         <View className='flex-1'>
-        <Text>AffirmationPractice</Text>
+            <ImageBackground source={affirmation?.image} resizeMode='cover' className='flex-1'>
+                <AppGradient colors={["rgba(0,0,0,0.3)", "rgba(0,0,0,0.9)"]}>
+                    <Text>Test</Text>
+                </AppGradient>
+            </ImageBackground>
         </View>
     )
 }
